c53c900544cad085b69f73bb4029c9c1e22fc109
root <root@maratiusure5.mylabserver.com>
2017-10-27 01:55:40 +0000

    remove node def

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,8 +1,3 @@
-node 'maratiusure4.mylabserver.com' {
-  notify { 'test-message-for maratiusure4':
-    message => 'This is maratiusure4 using a node def',
-  }
-}
 node default {
   hiera_include('classes')
 }
